db417dbf2ab3085732cea9a5e57e1a1b81ec5b30
Hyejin <hyejin.jeon940120@gmail.com>
2026-03-08 23:22:41 +0900

    [init] 프로젝트 초기 세팅 및 의존성 구성

---
+++ src/main/java/com/langdy/LangdyApplication.java
@@ -0,0 +1,12 @@
+package com.langdy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LangdyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LangdyApplication.class, args);
+    }
+}
---
+++ src/test/java/com/langdy/LangdyApplicationTests.java
@@ -0,0 +1,12 @@
+package com.langdy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LangdyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
